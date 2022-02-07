8b034f8562a65ae6931077cabf539d061859a8aa
gguidone <99129829+gguidone@users.noreply.github.com>
2022-02-07 19:59:59 +0100

    bella

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,21 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gguidone <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/06 18:17:09 by gguidone          #+#    #+#             */
-/*   Updated: 2022/02/07 18:11:42 by gguidone         ###   ########.fr       */
+/*   Created: 2022/02/07 17:24:14 by gguidone          #+#    #+#             */
+/*   Updated: 2022/02/07 18:12:55 by gguidone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+void	swap(int *xp, int *yp)
 {
-	int	l;
+	int	temp;
 
-	l = 0;
-	while (str[l] != 0)
-		l++;
-	return (l);
+	temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	min_idx;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		min_idx = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[min_idx])
+				min_idx = j;
+			j++;
+		}
+		swap(&tab[min_idx], &tab[i]);
+		i++;
+	}
 }
