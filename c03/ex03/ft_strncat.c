6aa4eaac51434042d03d4d39c7fb01184e24d02a
gguidone <99129829+gguidone@users.noreply.github.com>
2022-02-09 16:05:00 +0100

    esercizi fino a 4

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguidone <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 15:30:52 by gguidone          #+#    #+#             */
+/*   Updated: 2022/02/09 15:30:54 by gguidone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    *ft_strcat(char *dest, char *src, unsigned int nb)
+{
+    char *temp;
+
+    temp = dest;
+    while (*dest)
+        dest++;
+    while (*src && nb--)
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = 0;
+    return (temp);
+}
+
