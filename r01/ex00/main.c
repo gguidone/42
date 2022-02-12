35cacbd733067ec4a7607835d0e98a157dd0539b
gguidone <99129829+gguidone@users.noreply.github.com>
2022-02-12 17:16:04 +0100

    rush01 stampa la matrice!!!

--- r01/ex00/main.c
+++ r01/ex00/main.c
@@ -6,11 +6,12 @@
 /*   By: gguidone <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:13:59 by gguidone          #+#    #+#             */
-/*   Updated: 2022/02/12 13:24:11 by gguidone         ###   ########.fr       */
+/*   Updated: 2022/02/12 17:15:18 by gguidone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h>
 
 void ft_putchar(char c)
 {
@@ -59,43 +60,97 @@ int	 check_input(char *str, int lato)
 	return (1);
 }
 
-void get_strings(char *str)
+char *get_string(char *str, char *str2)
 {
 	int	cont;
-	int	cont2; 
 
-	cont2 = 0;
 	cont = 0;
 	while (str[cont] != '\0')
 	{
 		if (cont % 2 == 0)
 		{
-			
+			str2[cont / 2] = str[cont];
 		}
 		cont++;
-		cont2++;
+	}
+	str2[(cont + 1) / 2] = '\0';
+	return (str2);
+}
+
+char  **crea_matrice(char **mat, int lato)
+{
+	mat = (char **) malloc(lato * sizeof(char *));
+	int	i;
+
+	i = 0;
+	while (i < lato)
+	{
+		mat[i] = (char *) malloc(lato);
+		i++;
+	}
+	return (mat);
+}
+
+void riempi_matrice(char **mat, int lato)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (j < lato)
+	{
+		i = 0;
+		while (i < lato)
+		{
+			mat[j][i] = 48;
+			i++;
+		}
+		j++;
 	}
 }
-	
-void stampa_matrice(int lato)
-{
-	int	riga;
-	int	colonna;
 
-	riga = 0;
-	colonna = 0;
-	while (riga < lato)
+void stampa_matrice(char **mat, int lato)
+{
+	int	i;
+	int j;
+
+	j = 0;
+	while (j < lato)
 	{
-		colonna = 0;
-		while (colonna < lato)
+		i = 0;
+		while ( i < lato)
 		{
-			ft_putchar('0');
-			if (colonna != lato - 1)
-				ft_putchar(' ');
-			colonna++;
+			ft_putchar(mat[j][i]);
+			ft_putchar(' ');
+			i++;
 		}
 		ft_putchar('\n');
-		riga++;
+		j++;
+	}		
+}
+
+void put_string(char **mat, char *str)
+{
+	int	conts;
+	int contm;
+
+	conts = 0;
+	contm = 1;
+	while (str[conts] != '\0')
+	{
+		if (contm == 5)
+			contm = 1;
+		if (conts < 4)
+			mat [0][contm] = str[conts];
+		else if (conts < 8)
+			mat [5][contm] = str[conts];
+		else if (conts < 12)
+			mat [contm][0] = str[conts];
+		else 
+			mat [contm][5] = str[conts];
+		conts++;
+		contm++;
 	}
 }
 
@@ -108,7 +163,15 @@ int	main(int argc, char **argv)
 	{
 		flag = check_input(argv[1], 4);
 		if (flag == 1)
-			stampa_matrice(4);
+		{
+			char str[17]; 
+			get_string(argv[1], str);
+			char **mat = NULL;
+		   	mat = crea_matrice(mat, 6);
+			riempi_matrice(mat, 6);
+			put_string(mat, str);
+			stampa_matrice(mat, 6);
+		}
 	}	
 	return (0);
 }
