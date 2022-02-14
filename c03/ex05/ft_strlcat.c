904895030078987cd75466787d923ee097257dc9
gguidone <99129829+gguidone@users.noreply.github.com>
2022-02-14 15:49:05 +0100

    Add files via upload

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -9,3 +9,35 @@
 /*   Updated: 2022/02/09 15:31:44 by gguidone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int ft_strlen(char *c)
+{
+ int i;
+
+ i = 0;
+ while (str[i] != '\0')
+ {
+  i++;
+ }
+ return (i);
+}
+
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+{
+ int i;
+ int j;
+
+ j = 0;
+ i = ft_strlen(dest);
+ if (size - ft_strlen(dest) - 1 > 0)
+ {
+  while (i < size - ft_strlen(dest) && src[j] != '\0')
+  {
+   dest[i] = src[j];
+   i++;
+   j++;
+  }
+  dest[i] = '\0';
+ }
+ return (ft_strlen(src) + ft_strlen(dest));
+}
